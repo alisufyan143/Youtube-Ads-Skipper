0fe832b2a6bbab3dd9db6f2e757b1a674b47b5b1
alisufyan143 <49297226+alisufyan143@users.noreply.github.com>
2023-11-05 18:01:27 +0500

    Update background.js

--- background.js
+++ background.js
@@ -1,5 +1,16 @@
-chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-    if (changeInfo.status == 'complete' && tab.url.includes('https://www.youtube.com')) {
-      chrome.tabs.executeScript(tabId, {file: 'yourScript.js'});
+let isExtensionActive = true; // Variable to hold the state of the extension
+
+chrome.runtime.onMessage.addListener(
+  function(request, sender, sendResponse) {
+    if (request.message === "toggle") {
+      isExtensionActive = !isExtensionActive; // Toggle the state of the extension
     }
-  });
+    sendResponse({isActive: isExtensionActive});
+  }
+);
+
+chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+  if (changeInfo.status == 'complete' && tab.url.includes('https://www.youtube.com') && isExtensionActive) {
+    chrome.tabs.executeScript(tabId, {file: 'yourScript.js'});
+  }
+});
